76217e0e6966186e2b0f1c8f078453baec5467fe
g1257 <gz1@ornl.gov>
2015-01-06 16:31:17 -0500

    MatrixSpaceCollinear

--- src/Engine/SpinWave.h
+++ src/Engine/SpinWave.h
@@ -2,95 +2,53 @@
 #define SPINWAVE_H
 #include "String.h"
 #include <iostream>
-#include "SpaceConnectors.h"
-#include "Angles.h"
+#include "MatrixSpaceCollinear.h"
 
 namespace yasw {
 
 template<typename RealType, typename ComplexOrRealType>
 class SpinWave {
 
-	typedef yasw::SpaceConnectors<RealType,ComplexOrRealType> SpaceConnectorsType;
-	typedef yasw::Angles<RealType> AnglesType;
-
-	typedef typename SpaceConnectorsType::MatrixComplexOrRealType
-	MatrixComplexOrRealType;
-	typedef typename SpaceConnectorsType::VectorComplexOrRealMatrixType
-	VectorComplexOrRealMatrixType;
+	typedef MatrixSpaceCollinear<RealType,ComplexOrRealType> MatrixSpaceType;
+	typedef typename MatrixSpaceType::SpaceConnectorsType SpaceConnectorsType;
+	typedef typename MatrixSpaceType::AnglesType AnglesType;
+	typedef typename MatrixSpaceType::MatrixComplexOrRealType MatrixComplexOrRealType;
 
 public:
 
 	SpinWave(PsimagLite::String jfile,PsimagLite::String afile)
-	    : sc_(jfile),a_(afile),data_(sc_.size())
-	{
-		SizeType lda = sc_.rows();
-		for (SizeType i = 0; i < sc_.size(); ++i) {
-			MatrixComplexOrRealType m(2*lda,2*lda);
-			fillThisMatrix(m,i);
-			data_[i] = m;
-		}
-	}
+	    : matrixSpace_(jfile,afile)
+	{}
 
 	void printSpaceMatrices(std::ostream& os)
 	{
-		std::cerr<<"#Here are the "<<data_.size()<< " Hamiltonian-factor matrices.\n";
+		std::cerr<<"#Here are the "<<matrixSpace_.size();
+		std::cerr<< " Hamiltonian-factor matrices.\n";
 		std::cerr<<"-------------------------------------------------------------\n";
 		os<<"\n";
-		assert(data_.size() > 0);
-		os<<data_.size()<<" "<<data_[0].n_row()<<"\n";
-		for (SizeType i = 0; i < data_.size(); ++i) {
-			os<<sc_.nvector(i)<<"\n";
-			printMatrix(os,data_[i],1);
+		assert(matrixSpace_.size() > 0);
+		os<<matrixSpace_.size()<<" "<<matrixSpace_(0).n_row()<<"\n";
+		for (SizeType i = 0; i < matrixSpace_.size(); ++i) {
+			os<<matrixSpace_.nvector(i)<<"\n";
+			printMatrix(os,matrixSpace_(i),1);
 		}
 	}
 
 	void printDynamicMatrix(std::ostream& os)
 	{
-		std::cerr<<"#Here are the "<<data_.size()<< "Dynamic-factor matrices.\n";
+		std::cerr<<"#Here are the "<<matrixSpace_.size();
+		std::cerr<< "Dynamic-factor matrices.\n";
 		std::cerr<<"-------------------------------------------------------------\n";
 		os<<"\n";
-		assert(data_.size() > 0);
-		os<<data_.size()<<" "<<data_[0].n_row()<<"\n";
-		for (SizeType i = 0; i < data_.size(); ++i) {
-			os<<sc_.nvector(i)<<"\n";
-			printMatrix(os,data_[i],-1);
+		assert(matrixSpace_.size() > 0);
+		os<<matrixSpace_.size()<<" "<<matrixSpace_(0).n_row()<<"\n";
+		for (SizeType i = 0; i < matrixSpace_.size(); ++i) {
+			os<<matrixSpace_.nvector(i)<<"\n";
+			printMatrix(os,matrixSpace_(i),-1);
 		}
 	}
 
 private:
-
-	void fillThisMatrix(MatrixComplexOrRealType& m, SizeType ind) const
-	{
-		SizeType lda = sc_.rows();
-		for (SizeType i = 0; i < lda; ++i) {
-			for (SizeType j = 0; j < lda; ++j) {
-
-				if (a_(i,j) < 0)
-					m(i,j+lda) = m(i+lda,j) = sc_(i,j,ind);
-
-				if (a_(i,j) > 0)
-					m(i,j) = m(i+lda,j+lda) = sc_(i,j,ind);
-
-				if (sc_.isCentralCell(ind) && i == j)
-					m(i,i) = m(i+lda,i+lda) = diagonal(i);
-
-			}
-		}
-	}
-
-
-	ComplexOrRealType diagonal(SizeType i) const
-	{
-		ComplexOrRealType sum = 0;
-		SizeType lda = sc_.rows();
-		for (SizeType ind = 0; ind < sc_.size(); ++ind) {
-			for (SizeType j = 0; j < lda; ++j) {
-				sum += sc_(i,j,ind)*a_(i,j);
-			}
-		}
-
-		return sum;
-	}
 
 	void printMatrix(std::ostream& os,
 	                 const MatrixComplexOrRealType& m,
@@ -108,9 +66,7 @@ private:
 		}
 	}
 
-	SpaceConnectorsType sc_;
-	AnglesType a_;
-	VectorComplexOrRealMatrixType data_;
+	MatrixSpaceType matrixSpace_;
 }; // SpinWave
 
 }
